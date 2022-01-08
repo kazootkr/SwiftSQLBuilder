88d0d80e79d420cd3d19629b509b1466b5d01b93
kazootkr <kazoo.tkr@gmail.com>
2022-01-08 20:54:18 +0900

    SELECT文の生成ができるように

--- Sources/SwiftSQLBuilder/SwiftSQLBuilder.swift
+++ Sources/SwiftSQLBuilder/SwiftSQLBuilder.swift
@@ -1,6 +1,182 @@
-public struct SwiftSQLBuilder {
-    public private(set) var text = "Hello, World!"
+public protocol Table {
+    var tableName: String { get }
+}
 
-    public init() {
+@frozen public struct Query {
+    @inlinable public init(@Queryable content: () -> Query) {
+    }
+
+    /**
+     * DMLの種類を決定する役割をもつプロトコル
+     */
+    enum DMLDeterminer {
+        case select(columns: [String]? = nil, from: Table)
+        case update(from: Table)
+        case delete(from: Table)
+    }
+
+    struct Where: SQLClause {
+        let predicate: String
     }
 }
+
+@resultBuilder private struct Queryable {
+    static func buildBlock(_ determiner: Query.DMLDeterminer, _ clauses: SQLClause...) -> SQL {
+        var queryBuilder = QueryBuilder(components: SQLComponents(determiner: determiner, clauses: clauses));
+        queryBuilder.build()
+        return try! queryBuilder.getSQL()
+    }
+}
+
+// MARK: - private クエリビルダー自体は外から見えないように
+
+/**
+ * SQL文の構成要素を含むコレクションオブジェクト
+ */
+struct SQLComponents {
+    let determiner: Query.DMLDeterminer
+    let clauses: [SQLClause]
+
+    init(determiner: Query.DMLDeterminer, clauses: [SQLClause] = []) {
+        self.determiner = determiner
+        self.clauses = clauses
+    }
+
+    func getClause<T: SQLClause>(kind: T.Type) -> [T]? {
+        guard !clauses.isEmpty else {
+            return nil
+        }
+
+        var result: [T] = []
+
+        for clause in clauses {
+            if clause is T {
+                if let cast = clause as? T {
+                    result.append(cast)
+                }
+            }
+        }
+
+        return result
+    }
+}
+
+extension Query.DMLDeterminer {
+    func toSQLString() -> String {
+        switch (self) {
+        case let Query.DMLDeterminer.select(columns, from):
+            let unwrappedColumns: [String] = columns ?? ["*"]
+            return "SELECT \(unwrappedColumns.joined(separator: ", ")) FROM \(from.tableName)"
+        case let Query.DMLDeterminer.update(from):
+            return "UPDATE FROM \(from.tableName)"
+        case let Query.DMLDeterminer.delete(from):
+            return "DELETE FROM \(from.tableName)"
+        }
+    }
+}
+
+/**
+ SQL文を構成する最小単位
+ */
+protocol SQLClause {
+}
+
+struct SQL {
+    let rawValue: String
+}
+
+protocol ConcreteBuilder {
+    mutating func build()
+    func getSQL() throws -> SQL
+}
+
+struct QueryBuilder {
+    var sql: SQL?
+
+    var components: SQLComponents
+
+    init(components: SQLComponents) {
+        self.components = components
+    }
+
+    mutating func build() {
+        var concreteBuilder = getBuilder()
+        concreteBuilder.build()
+        sql = try! concreteBuilder.getSQL()
+    };
+
+    enum InternalError: Error {
+        case noParams
+        case noBuild
+    }
+
+    func getSQL() throws -> SQL {
+        guard let unwrappedSQL = sql else {
+            throw InternalError.noBuild
+        }
+
+        return unwrappedSQL
+    }
+
+    private func getBuilder() -> ConcreteBuilder {
+        switch components.determiner {
+        case .select:
+            return SelectQueryBuilder(components: components)
+        case .update:
+            return UpdateQueryBuilder()
+        case .delete:
+            return DeleteQueryBuilder()
+        }
+    }
+
+    struct SelectQueryBuilder: ConcreteBuilder {
+        let determiner: Query.DMLDeterminer
+        var sqlWhere: [Query.Where]? = nil
+
+        var result: SQL?
+
+        init(components: SQLComponents) {
+            determiner = components.determiner
+            sqlWhere = components.getClause(kind: Query.Where.self)
+        }
+
+        mutating func build() {
+            var sqlString: String = ""
+            sqlString += determiner.toSQLString()
+
+            if let unwrappedSqlWhere: [Query.Where] = sqlWhere {
+                sqlString += " WHERE \(unwrappedSqlWhere.map { $0.predicate }.joined(separator: " AND "))"
+            }
+
+            result = SQL(rawValue: sqlString)
+        }
+
+        func getSQL() throws -> SQL {
+            guard let unwrappedSQL = result else {
+                throw QueryBuilder.InternalError.noBuild
+            }
+            return unwrappedSQL
+        }
+    }
+
+    struct UpdateQueryBuilder: ConcreteBuilder {
+        mutating func build() {
+        }
+
+        func getSQL() -> SQL {
+            SQL(rawValue: "")
+        }
+    }
+
+    struct DeleteQueryBuilder: ConcreteBuilder {
+        mutating func build() {
+        }
+
+        func getSQL() -> SQL {
+            SQL(rawValue: "")
+        }
+    }
+}
+
+
+
--- Tests/SwiftSQLBuilderTests/SwiftSQLBuilderTests.swift
+++ Tests/SwiftSQLBuilderTests/SwiftSQLBuilderTests.swift
@@ -1,11 +1,90 @@
 import XCTest
 @testable import SwiftSQLBuilder
 
-final class SwiftSQLBuilderTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftSQLBuilder().text, "Hello, World!")
+final class SelectQueryBuilderTest: XCTestCase {
+    func testBuild_最低限のSELECT文が生成できる() throws {
+        var builder1 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(from: makeFriend())))
+        builder1.build()
+        XCTAssertEqual("SELECT * FROM friends", try! builder1.getSQL().rawValue)
+
+        var builder2 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(from: makeBook())))
+        builder2.build()
+        XCTAssertEqual("SELECT * FROM books", try! builder2.getSQL().rawValue)
+    }
+
+    func testBuild_カラム指定のSELECT文を生成() throws {
+        var builder1 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "name"], from: makeFriend())))
+        builder1.build()
+        XCTAssertEqual("SELECT id, name FROM friends", try! builder1.getSQL().rawValue)
+
+
+        var builder2 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "title", "publisher"], from: makeBook())))
+        builder2.build()
+        XCTAssertEqual("SELECT id, title, publisher FROM books", try! builder2.getSQL().rawValue)
+    }
+
+    func testBuild_カラム指定のSELECT文を生成_カラム名の指定が誤っている_例外() throws {
+        throw XCTSkip("未実装")
+        var builder1 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "no_column_name"], from: makeFriend())))
+
+        XCTAssertThrowsError(builder1.build())
+        XCTAssertEqual("SELECT id, name FROM friends", try! builder1.getSQL().rawValue)
+
+
+        var builder2 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "title", "publisher"], from: makeBook())))
+        builder2.build()
+        XCTAssertEqual("SELECT id, title, publisher FROM books", try! builder2.getSQL().rawValue)
+    }
+
+    func testBuild_条件付きのSELECT文が生成できる() throws {
+        var builder1 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(from: makeFriend()), clauses: [Query.Where(predicate: "id = 1")]))
+        builder1.build()
+        XCTAssertEqual("SELECT * FROM friends WHERE id = 1", try! builder1.getSQL().rawValue)
+
+        var builder2 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "title"], from: makeBook()), clauses: [Query.Where(predicate: "title like 'swift book'")]))
+        builder2.build()
+        XCTAssertEqual("SELECT id, title FROM books WHERE title like 'swift book'", try! builder2.getSQL().rawValue)
+
+        var builder3 = QueryBuilder(components: SQLComponents(determiner: Query.DMLDeterminer.select(columns: ["id", "title"], from: makeBook()), clauses: [Query.Where(predicate: "id = 1"), Query.Where(predicate: "title like 'swift book'")]))
+        builder3.build()
+        XCTAssertEqual("SELECT id, title FROM books WHERE id = 1 AND title like 'swift book'", try! builder3.getSQL().rawValue)
+    }
+}
+
+    func makeFriend() -> Friend
+    {
+        Friend(id: 1, name: "taro", age: 20)
+    }
+
+    func makeBook() -> Book
+    {
+        Book(id: 10, title: "swift book", publisher: "book maker")
+    }
+
+class Friend: Table {
+    var tableName: String = "friends"
+
+    var id: Int
+    var name: String
+    var age: Int
+
+    init(id: Int, name: String, age: Int) {
+        self.id = id
+        self.name = name
+        self.age = age
+    }
+}
+
+class Book: Table {
+    var tableName: String = "books"
+
+    var id: Int
+    var title: String
+    var publisher: String
+
+    init(id: Int, title: String, publisher: String) {
+        self.id = id
+        self.title = title
+        self.publisher = publisher
     }
 }
